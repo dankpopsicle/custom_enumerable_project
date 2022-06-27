4b3ce048ebc0bc4bdd3517f7db3268fb77b16b33
Adam Taber <altaber@gmail.com>
2022-06-27 17:12:45 -0500

    Add other methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -27,6 +27,65 @@ module Enumerable
     return false
   end
 
+  def my_count
+    if block_given?
+      count = 0
+      my_each do |x|
+        if (yield x) == true
+          count += 1
+        end
+      end
+      return count
+    else
+      return self.length
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield((self[i]), i)
+      i += 1
+    end
+    self
+  end
+
+  def my_inject(init_value)
+    sum = init_value
+    my_each do |x|
+      sum = (yield(sum, x))
+    end
+    sum
+  end
+
+  def my_map
+    array = []
+    my_each do |x|
+      array << (yield x)
+    end
+    array
+  end
+  
+  def my_none?
+    my_each do |x|
+      if (yield x) == true
+        return false
+      end
+    end
+    return true
+  end
+
+  def my_select
+    array = []
+    my_each do |x|
+      if (yield x) == true
+        array << x
+      end
+    end
+    array
+  end
+
+
 end
 
 # You will first have to define my_each
