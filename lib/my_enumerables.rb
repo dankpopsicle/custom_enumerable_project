4737a6ae736b77fe5099d3f89dceb7118d8bd799
Adam Taber <altaber@gmail.com>
2022-06-27 15:43:23 -0500

    Add my_all? method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,18 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    i = 0
+    while i < self.length
+      yield self[i]
+      if (yield self[i]) == false
+        return false
+        i = self.length
+      elsif i == (self.length - 1) && (yield self[i]) == true
+        return true
+      else 
+        i += 1
+      end
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +20,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    while i < self.length
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
